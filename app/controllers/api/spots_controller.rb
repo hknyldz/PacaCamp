71a530dc824682712de7fc60bf54547c3f50e3af
Aimy <aimy1yu@gmail.com>
2020-05-13 08:42:11 -0400

    Edit index action for spot search functionality

--- app/controllers/api/spots_controller.rb
+++ app/controllers/api/spots_controller.rb
@@ -1,6 +1,10 @@
 class Api::SpotsController < ApplicationController
     def index 
-        @spots = Spot.all.includes(:area, :essential, :amenity, :detail).with_attached_photos
+        if params[:bounds]
+            @spots = Spot.all.includes(:area, :essential, :amenity, :detail).with_attached_photos.select { |spot| spot.in_bounds(params[:bounds]) }
+        else 
+            @spots = Spot.all.includes(:area, :essential, :amenity, :detail).with_attached_photos
+        end 
     end
 
     def show
