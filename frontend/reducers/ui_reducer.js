8e8ce82346cb8c63fdfbbf630f21f6960c372d42
Aimy <aimy1yu@gmail.com>
2020-05-13 11:45:32 -0400

    Add boundsFilterReducer to uiReducer

--- frontend/reducers/ui_reducer.js
+++ frontend/reducers/ui_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import modalReducer from './modal_reducer';
+import boundsFilterReducer from './bounds_filter_reducer';
 
 const uiReducer = combineReducers({
-    modal: modalReducer
+    modal: modalReducer,
+    boundsFilter: boundsFilterReducer,
 });
 
 export default uiReducer;
