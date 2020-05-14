249bbb8794fac420e67d63258c917220e985b03a
Aimy <aimy1yu@gmail.com>
2020-05-14 17:36:51 -0400

    Render SearchIndexItem

---
+++ frontend/components/search/search_index_item.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { withRouter } from "react-router-dom";
+
+const SearchIndexItem = props => {
+    return <div>SearchIndexItem</div>;
+}
+
+export default withRouter(SearchIndexItem);
