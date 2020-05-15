ecd17a15c3213091a0dd2fa7c548ff35ef636bea
Aimy <aimy1yu@gmail.com>
2020-05-15 17:32:44 -0400

    Remove testing comments

--- frontend/pacacamp.jsx
+++ frontend/pacacamp.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-import { signup, login, logout } from './util/session_api_util'; //TESTING
-import { fetchSpots, fetchSpot } from './actions/spot_actions'; //TESTING
-import { fetchReviews, createReview, updateReview, deleteReview } from './util/review_api_util'; //TESTING
 
 document.addEventListener("DOMContentLoaded", () => {
     let store;
@@ -20,20 +17,6 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
         store = configureStore();
     }
-
-    //TESTING
-    window.signup = signup;
-    window.login = login;
-    window.logout = logout;
-    window.getState = store.getState;
-    window.dispatch = store.dispatch;
-    window.fetchSpots = fetchSpots;
-    window.fetchSpot = fetchSpot;
-    window.fetchReviews = fetchReviews; 
-    window.createReview = createReview;
-    window.updateReview = updateReview;
-    window.deleteReview =  deleteReview;
-    //TESTING
     
     const rootEl = document.getElementById("root");
     ReactDOM.render(<Root store={store} />, rootEl);
